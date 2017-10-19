164af14fa3cbba1d639bdc0921d92d5f0216d388
Yuriy_Kovalchuk <Yuriy.Kovalchuk@symphony-solutions-eu>
2017-10-19 14:15:07 +0300

    script updated with addNewMachine method

--- script.js
+++ script.js
@@ -21,9 +21,16 @@ function SlotMachine(MachineMoneyAmount){
 Casino.prototype.getTotalAmount = function(){
   var totalAmount = 0;
   for (i=0; i<this.SlotMachines; i++){
-    totalAmount+=this.Machines;
+    totalAmount+=this.Machines[i].MachineMoneyAmount;
   }
+  document.querySelector('#money-in-casino').innerHTML=totalAmount;
   console.log(totalAmount)
+
+}
+Casino.prototype.addNewMachine = function (){
+  console.log(parseInt(this.SlotMachines));
+  this.SlotMachines = parseInt(this.SlotMachines)+1;
+  console.log(parseInt(this.SlotMachines));
 }
 
 function createCasino(form){
@@ -31,7 +38,7 @@ function createCasino(form){
   // document.body.insertAdjacentHTML('beforeEnd', '<section class="casino-section"><p>two</p></section>');
   casino = new Casino(form[0].value, form[1].value);
   playerMoney = casino.CasinoMoneyAmount;
-  document.body.insertAdjacentHTML('beforeEnd', '<section class="casino-section">     <div class="casino-cnt casinomoney-cnt">    <h3>Money in casino - $<span id="money-in-casino">'+casino.CasinoMoneyAmount+'</span></h3><h3>Player&#39;s money - $<span>'+playerMoney+'</span></h3>   </div>  <div class="casino-cnt add-slot-cnt">   <p>Add new machine</p>  </div>  <div class="casino-cnt slots-cnt">  </div> </section>')
+  document.body.insertAdjacentHTML('beforeEnd', '<section class="casino-section">     <div class="casino-cnt casinomoney-cnt">    <h3>Money in casino - $<span id="money-in-casino">'+casino.CasinoMoneyAmount+'</span></h3><h3>Player&#39;s money - $<span>'+playerMoney+'</span></h3>   </div>  <div class="casino-cnt add-slot-cnt" onclick="casino.addNewMachine()"">   <p>Add new machine</p>  </div>  <div class="casino-cnt slots-cnt">  </div> </section>')
     for (var i=0; i<casino.SlotMachines; i++){
     document.querySelector('.slots-cnt').insertAdjacentHTML('beforeEnd', '<div class="img-cnt">  <img src="img/slot-machine.png" alt="slot-machine">   <p class="number-on-slot">777</p>     <p class="play-button" onclick="casino.getTotalAmount()">PLAY</p>    <p class="slot-money-field">$<span>'+casino.Machines[i].MachineMoneyAmount+'</span></p>     <p class="delete-button">x</p>    <p class="bet-cnt">Bet <select name="bet">             <option value="1">$1</option>        <option value="5">$5</option>       <option value="10">$10</option>       <option value="50">$50</option>        <option value="100">$100</option>      <option value="500">$500</option>    </select></p>    </div>');
   }
